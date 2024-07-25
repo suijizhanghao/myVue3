5c9375cd0d24e3ab3a0be5f700c1ddaffe33d972
lipt <suijizhanghao@126.com>
2024-07-25 23:20:30 +0800

    @home

--- 00-ECMA6/01/06.js
+++ 00-ECMA6/01/06.js
@@ -5,3 +5,34 @@ a.addredss3 = "3";
 for (let key in a) {
     console.log(key, a[key]);
 }
+
+let o1 = Object.create({x:1, y: 2})
+console.log(o1.prototype)
+
+let o = {x:1, y:{z: [false, null, ""]}, z : new Date()}
+let s= JSON.stringify(o);
+console.log(s);
+let p = JSON.parse(s);
+console.log(p);
+console.log(typeof (p.z));
+
+console.log([1,2,3].toLocaleString())
+
+console.log([1,2,3].toString())
+console.log({o,s})
+
+const PROPERTY_NAME = "p1";
+function computePropertyName(){
+    return "p" + 2;}
+
+let o2 = {}
+o2[computePropertyName()] = 1;
+o2[PROPERTY_NAME] = 2
+console.log(o2)
+let o3 = {
+    [computePropertyName()]: 1,
+    [PROPERTY_NAME]: 2
+}
+console.log(o3)
+
+// P150  6.10.3
