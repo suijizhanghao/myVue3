02d11f7eb9fbcdf364326539d98c837895bea29a
lipt <suijizhanghao@126.com>
2024-09-14 21:04:31 +0800

    @home

--- 00-ECMA6/01/11.js
+++ 00-ECMA6/01/11.js
@@ -1,3 +1,46 @@
+let s = new Set();
+let t = new Set([1, 2, 3]);
+let unique = new Set('Mississippi');
+console.log(unique)
+console.log(unique.keys())
+unique.add('1').add('2').add('3')
+console.log(unique)
+console.log(unique.has('Mississippi'))
+for (let i of unique) {
+    console.log(i);
+}
+console.log(...unique)
+console.log(new Array(...unique));
+unique.forEach((key) => console.log(key+'a'));
+
+let m = new Map();
+let m1 = new Map([['1', 111], ['2', 222]]);
+console.log(m1)
+let o = {a:1, y: 2}
+console.log(Object.entries(o))
+let m2 = new Map(Object.entries(o))
+console.log(m2)
+console.log(m2.get('a'));
+console.log(m2.set('c', 333));
+console.log(m2);
+console.log(m2.delete('c'));
+console.log({} == {});
+console.log({} === {});
+
+console.log(...m2);
+for (let [key, value] of m2) {
+    console.log(key, value);
+}
+
+console.log(...m2.entries())
+
+m2.forEach((value, key) => console.log(key, value));
+
+//--------------------------
+let a1 = new Int8Array(10);
+console.log(a1);
+a1[1]=11111111111111111;
+console.log(a1);
 
 //11.3以前的部分
 let pattern = /s$/;
