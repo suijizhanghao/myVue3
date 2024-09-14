9d15c82a2b3597c1c39363643e4f645f56ae1003
myfun <suijizhanghao@126.com>
2024-09-14 18:26:08 +0800

    @cib

--- 00-ECMA6/01/11.js
+++ 00-ECMA6/01/11.js
@@ -6,3 +6,8 @@ pattern = new RegExp("s$");
 
 let p1 = /([Jj]ava(?:[Ss]cript)?)\sis\s(fun\w*)/;
 console.log(p1.test("JavaScript is funnnn"));
+
+console.log('JavaScriptScript'.search(/S/))
+console.log('javascript abcd'.replace(/(?<n1>javascript)/gi, "$<n1> ==> JavaScript"))
+
+'1 + 1 = 2'.replace()
